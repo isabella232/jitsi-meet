ad740a3ec022b871b87ea65a7b89e89f9e99f52d
Vlad Piersec <vlad.piersec@8x8.com>
2020-07-15 13:07:10 +0300

    fix(prejoin): Show decoded version of meeting uri

--- react/features/invite/components/add-people-dialog/web/CopyMeetingLinkSection.js
+++ react/features/invite/components/add-people-dialog/web/CopyMeetingLinkSection.js
@@ -4,7 +4,8 @@ import React from 'react';
 
 import CopyButton from '../../../../base/buttons/CopyButton';
 import { translate } from '../../../../base/i18n';
-import { getDecodedURI } from '../../../../base/util';
+import { Icon, IconCheck, IconCopy } from '../../../../base/icons';
+import { copyText, getDecodedURI } from '../../../../base/util';
 
 
 type Props = {
@@ -26,6 +27,74 @@ type Props = {
  * @returns {React$Element<any>}
  */
 function CopyMeetingLinkSection({ t, url }: Props) {
+    const [ isClicked, setIsClicked ] = useState(false);
+    const [ isHovered, setIsHovered ] = useState(false);
+
+    /**
+     * Click handler for the element.
+     *
+     * @returns {void}
+     */
+    function onClick() {
+        setIsHovered(false);
+        if (copyText(url)) {
+            setIsClicked(true);
+
+            setTimeout(() => {
+                setIsClicked(false);
+            }, 2500);
+        }
+    }
+
+    /**
+     * Hover handler for the element.
+     *
+     * @returns {void}
+     */
+    function onHoverIn() {
+        if (!isClicked) {
+            setIsHovered(true);
+        }
+    }
+
+    /**
+     * Hover handler for the element.
+     *
+     * @returns {void}
+     */
+    function onHoverOut() {
+        setIsHovered(false);
+    }
+
+    /**
+     * Renders the content of the link based on the state.
+     *
+     * @returns {React$Element<any>}
+     */
+    function renderLinkContent() {
+        if (isClicked) {
+            return (
+                <>
+                    <div className = 'invite-more-dialog copy-link-text selected'>
+                        {t('addPeople.linkCopied')}
+                    </div>
+                    <Icon src = { IconCheck } />
+                </>
+            );
+        }
+
+        const displayUrl = getDecodedURI(url);
+
+        return (
+            <>
+                <div className = 'invite-more-dialog invite-more-dialog-conference-url copy-link-text'>
+                    {isHovered ? t('addPeople.copyLink') : displayUrl}
+                </div>
+                <Icon src = { IconCopy } />
+            </>
+        );
+    }
+
     return (
         <>
             <span>{t('addPeople.shareLink')}</span>
